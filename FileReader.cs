e2a3c3d2f3e32c27d1263202c0e0fd02649f27ef
stas <79771234+ninahur1989@users.noreply.github.com>
2022-06-16 16:51:16 +0300

    Finish

---
+++ FileReader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace Asynchrony
+{
+    public class FileReader
+    {
+        public string Text { get; set; }
+
+        public async Task<string> HelloReader()
+        {
+            return await Task.Run(() =>
+            {
+                var json = JsonConvert.DeserializeObject<FileReader>(File.ReadAllText(@"C:\Users\Admin\source\repos\Asynchrony\Asynchrony\hello_json.json"));
+                return json.Text;
+            });
+        }
+
+        public async Task<string> WorldReader()
+        {
+            return await Task.Run( () =>
+            {
+                var json = JsonConvert.DeserializeObject<FileReader>(File.ReadAllText(@"C:\Users\Admin\source\repos\Asynchrony\Asynchrony\world_json.json"));
+                return json.Text;
+            });
+        }
+
+        public async Task<string> Adder()
+        {
+            Console.WriteLine("start adder");
+            return await Task.Run( () =>
+            {
+                
+                return HelloReader().GetAwaiter().GetResult() + " " + WorldReader().GetAwaiter().GetResult();
+            });
+        }
+    }
+}
